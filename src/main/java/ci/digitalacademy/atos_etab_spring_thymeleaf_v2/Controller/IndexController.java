40aad42f1088af2f351f651d56ec4e3769414deb
delmas <azertyazerty1ze@gmail.com>
2024-09-02 18:50:00 +0000

    config smtp et school

--- src/main/java/ci/digitalacademy/atos_etab_spring_thymeleaf_v2/Controller/IndexController.java
+++ src/main/java/ci/digitalacademy/atos_etab_spring_thymeleaf_v2/Controller/IndexController.java
@@ -28,22 +28,6 @@ public class IndexController {
 
     @GetMapping
     public  String verifyConfiguration() {
-//        AppSettingDTO appSettingDTO = new AppSettingDTO();
-//        appSettingDTO.setSmtpPassword("password");
-//        appSettingDTO.setSmtpPort(8080L);
-//        appSettingDTO.setSmtpUsername("username");
-//        appSettingDTO.setSmtpServer("host");
-//        System.out.println(appSettingDTO);
-//        AppSettingDTO settingDTO = appService.initApp(appSettingDTO);
-//
-//        SchoolDTO schoolDTO = new SchoolDTO();
-//        schoolDTO.setName("Ecole de formation");
-//        schoolDTO.setLogo("https://www.google.com/url?sa=i&url=https%3A%2F%2Fville-saint-gobain.fr%2Fpage-daccueil%2Flogo-ecole-2%2F&psig=AOvVaw2RGcw1mpgHA2w8aO9S6gAE&ust=1725357364930000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIit0Jr_o4gDFQAAAAAdAAAAABAE");
-//        SchoolDTO school = appService.initSchool(schoolDTO,settingDTO);
-
-
-
-
         List<AppSettingDTO> appSettingDTOS = appSettingService.getAll();
         List<SchoolDTO> schoolDTOS = schoolService.getAll();
         if (appSettingDTOS.isEmpty()) {
